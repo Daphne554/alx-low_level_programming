ec7fcf0a4afc3232e5bd17177aa358ea49185eb4
Daphne554 <68458849+Daphne554@users.noreply.github.com>
2022-12-13 21:19:50 +0000

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -41,9 +41,9 @@ int main(void)
 			n2 = fn;
 			n22 = (fn2 % 1000000000);
 		}
-		if (((n1 + n2) < 0) && boolean == 1)
-			boolean = 0;
+		if (((n1 + n2) < 0) && bool1 == 1)
+			bool1 = 0;
 	}
 	printf("\n");
-	return (00);
+	return (0);
 }
