67a575f671f2cea009cc052adaf23b6b2d292de8
Daphne554 <daphnekodua@gmail.com>
2022-12-13 05:15:12 -0800

    shows lowercase char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Shows 1 if input is lowercase, other case shows 0.
+ *
+ * Return: 1 (Success for lowercase) 0 (Success for other)
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('/n');
+}	
