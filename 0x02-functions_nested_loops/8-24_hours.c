fd34022685bbec90481f2be655cdb0f91385703c
Daphne554 <68458849+Daphne554@users.noreply.github.com>
2022-12-13 14:22:51 +0000

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,15 +15,18 @@ void jack_bauer(void)
 		{
 			for (c = 48; c <= 53; c++)
 			{
-				if (a >= 50 && b >= 52)
-				_putchar(a);
-				_putchar(b);
-				_putchar(58);
-				_putchar(c);
-				_putchar(d);
-				_putchar('\n');
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
 			}
 		}
 	}
-}
 }
