5958bc4664dd22c2eb8e0fe19deca05f11fd2dee
Daphne554 <68458849+Daphne554@users.noreply.github.com>
2022-12-13 20:32:54 +0000

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,8 +12,8 @@ int main(void)
 
 	n1 = 1;
 	n2 = 2;
-	printf("%ld, %ld, n1, n2);
-	for (c = 0;c  48; c++)
+	printf("%ld, %ld", n1, n2);
+	for (c = 0;c < 48; c++)
 	{
 		fn = n1 + n2;
 		printf(", %ld", fn);
