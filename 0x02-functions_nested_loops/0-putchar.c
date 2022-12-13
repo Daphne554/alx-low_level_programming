597275cdd0ccf43fc062defe59fad83b489ac7b0
Daphne554 <68458849+Daphne554@users.noreply.github.com>
2022-12-13 18:27:12 +0000

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,16 @@
  *
  * Return: 1 (Success)
  */
-
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
