9a8b92dd931a153ac5cd37f87db36942febd3d36
Daphne554 <daphnekodua@gmail.com>
2022-12-14 08:27:34 -0800

    debug prototype (positive or negative)

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - test function that prints positive or negative integer
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = o;
+	positive_or_negative(i);
+
+
+	return (0);
+}
