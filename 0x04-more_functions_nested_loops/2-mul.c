06c86d45e5eadbd9b5cbfe9aade74e46681a0b62
Daphne554 <daphnekodua@gmail.com>
2022-12-15 11:11:07 -0800

    prints numbers between 0 and 9

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiply integer
+ * @a: first int
+ * @b: second int
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
