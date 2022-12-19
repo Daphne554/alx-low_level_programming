6b7051f51e5ae54cdc55c683d37f7733576415ec
Daphne554 <daphnekodua@gmail.com>
2022-12-19 07:16:26 -0800

    print string wih new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - prints string with new line
+ * @str: string input
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
+	}
+}
